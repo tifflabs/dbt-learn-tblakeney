3956fa8a5e52c651fb0dcee8657b526fab90b20b
Tiffany Blakeney <tiffany@philo.com>
2021-02-10 22:10:25 +0000

    ltv added to customer data

--- models/customers.sql
+++ models/customers.sql
@@ -33,6 +33,7 @@ final as (
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
+        customers.ltv,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,6 +1,8 @@
 select
-    id as customer_id,
-    first_name,
-    last_name
-
-from raw.jaffle_shop.customers
+    a.id as customer_id,
+    a.first_name,
+    a.last_name,
+    b.orderid as order_id,
+    b.amount 
+from raw.jaffle_shop.customers as a left join raw.stripe.payment as b on a.id=b.id
+where b.status like '%success%'
